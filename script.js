5193368f60f53ab1dd867404b798087669998a04
Luka Popovic <lukapopovic0111@gmail.com>
2025-06-10 22:44:00 +0200

    Making variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-"use strict";
+'use strict';
+const availabilityValue = document.querySelector('.availability');
+const carsBox = document.querySelector('.cars');
+const sort = document.querySelector('.sortOptions');
+/* */
